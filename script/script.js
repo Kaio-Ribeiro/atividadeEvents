600a154df202117ce3dd72ac94cd152f3e135e5a
Kaio-Ribeiro <51684209+Kaio-Ribeiro@users.noreply.github.com>
2020-03-11 14:13:10 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,23 +1,35 @@
-const salvarTarefa = function(){
+const criarTarefa = function(){
 	const novaTarefa = document.getElementById('novaTarefa').value
 
 	let lista = document.getElementById('listaFazer')
 	let li = document.createElement('li')
+	let label = document.createElement('label')
 	let input = document.createElement('input')
 	input.type = "checkbox"
 
-	li.append(input)
-	li.append(novaTarefa)
+	li.append(label)
+	label.append(input)
+	label.append(novaTarefa)
 	lista.append(li)
-}
 
+	input.addEventListener('click', function(){
+		let listaFeitos = document.getElementById('listaFeitos')
 
-const excluirTarefa = function(){
-	let lista = document.getElementById('listaFazer')
-	let listaFeitos = document.getElementById('listaFeitos')
+		input.type = "hidden"
+		listaFeitos.append(li)
 
+		let botaoExcluir = document.createElement('button')
+		botaoExcluir.type = "button"
+		let textoButao = document.createTextNode('x')
 
+		botaoExcluir.append(textoButao)
+		li.append(botaoExcluir)
+
+		botaoExcluir.addEventListener('click', function(){
+			listaFeitos.removeChild(li)
+		})
+	})
 }
 
 let butao = document.getElementById('butao')
-	butao.addEventListener('click', salvarTarefa)
+butao.addEventListener('click', criarTarefa)
